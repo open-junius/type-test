c2958bb933f2732c6904110294bee5805c617870
open-junius <zhou@opentensor.dev>
2025-02-26 00:08:26 +0800

    remove main function

--- src/main.ts
+++ src/main.ts
@@ -12,38 +12,6 @@ import { ethers } from "ethers"
 
 async function main() {
 
-    // const subClient = await getClient(SUB_LOCAL_URL)
-    // api = await getDevnetApi(subClient)
-    // alice = await getAliceSigner();
-
-    // const wallet = generateRandomEthersWallet();
-    // const wallet2 = generateRandomEthersWallet();
-    // const publicClient = await getPublicClient(ETH_LOCAL_URL)
-    // const provider = new ethers.JsonRpcProvider(ETH_LOCAL_URL);
-
-    // const transferBalance = raoToEth(tao(1))
-
-    // // console.log(sendBalance, receiverBalance, transferBalance, transferBalance.toString())
-
-    // const tx = {
-    //     to: wallet2.address,
-    //     amount: transferBalance.toString()
-    // }
-
-    // const hash = await wallet.sendTransaction(tx)
-
-    // console.log(hash)
-
-    // await hash.wait();
-
-    // await new Promise(resolve => setTimeout(resolve, 1000));
-
-
-    // const a = await provider.getBalance(wallet.address)
-    // const b = await provider.getBalance(wallet2.address)
-
-    // console.log("a and b", a, b)
-
 }
 
 main();
